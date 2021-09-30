a1fd9e8befaa937f697102efe940995dabdffc64
Vishal Garad <garad_v@India.XoriantCorp.com>
2021-09-30 12:13:25 +0530

    second commit

--- src/test/java/com/jenkins/SpringJenkins1ApplicationTests.java
+++ src/test/java/com/jenkins/SpringJenkins1ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.jenkins;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class SpringJenkins1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
+	
